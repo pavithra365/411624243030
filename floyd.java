92a2811c59cf632642aea16c85e21dd85cecb212
pavithra365 <s.pavithra.aids28@princedrkvasudevan.com>
2026-06-08 11:00:41 +0530

    1st

---
+++ floyd.java
@@ -0,0 +1,19 @@
+public class floyd {
+    public static void main(String[] args) {
+        int num=0;
+        
+        
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num);
+                num=num+2;
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
+
+    
+
